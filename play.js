c21668be43668a36cae6723c6a79372b08a12997
Rebekah <rebekahmparkes@outlook.com>
2021-06-30 17:25:49 +0000

    client and using module.exports

--- play.js
+++ play.js
@@ -1,17 +1,3 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-
+const { net, connect } = require("./client");
 console.log("Connecting ...");
 connect();
